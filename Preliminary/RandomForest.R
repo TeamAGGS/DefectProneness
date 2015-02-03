12c9d2c88dc0dd6fb236222ea91004af2d5f3a1b
Gaurav Saraf <grv_srf@yahoo.co.in>
2015-02-02 23:43:46 -0500

    Subsetting of the dataset into 5 subsets done

--- Preliminary/RandomForest.R
+++ Preliminary/RandomForest.R
@@ -2,30 +2,41 @@
 library(randomForest) 
 set.seed(123)
 dataset <- read.csv(file="data/xerces-1.2.csv", na.strings=c(".", "NA", "", "?"), strip.white=TRUE, encoding="UTF-8")
+
+########################### Pre-processing ###########################
+
 dataset <- dataset[,-c(1,2,3)]
-dataset.buggy <- dataset[which(dataset$bug > 0),]
-dataset.notbuggy <- dataset[which(dataset$bug == 0),]
-dataset.buggy <- dataset.buggy[,-ncol(dataset.buggy)]
-dataset.notbuggy <- dataset.notbuggy[,-ncol(dataset.notbuggy)]
-classes <- c(rep(1,nrow(dataset.buggy)))
-dataset.buggy <- cbind(dataset.buggy, classes)
-classes <- c(rep(0,nrow(dataset.notbuggy)))
-dataset.notbuggy <- cbind(dataset.notbuggy, classes)
+bugs <- rep(0,nrow(dataset))
+bugs[which(dataset$bug > 0)] <- 1
+dataset <- dataset[,-ncol(dataset)]
+dataset <- cbind(dataset,bugs)
 
-train.buggy <- sample(1:nrow(dataset.buggy), 0.70*nrow(dataset.buggy), replace = F)
-train.notbuggy <- sample(1:nrow(dataset.notbuggy), 0.70*nrow(dataset.notbuggy), replace = F)
-test.buggy <- setdiff(1:nrow(dataset.buggy), train.buggy)
-test.notbuggy <- setdiff(1:nrow(dataset.notbuggy), train.notbuggy)
+############################# Partition ##############################
+subset.size <- abs(0.20*nrow(dataset))
+subset1 <- sample(1:nrow(dataset), subset.size, replace=F)
+remaining <- setdiff(1:nrow(dataset),subset1)
+subset2 <- sample(remaining, subset.size, replace=F)
+remaining <- setdiff(remaining, subset2)
+subset3 <- sample(remaining, subset.size, replace=F)
+remaining <- setdiff(remaining, subset3)
+subset4 <- sample(remaining, subset.size, replace=F)
+remaining <- setdiff(remaining, subset4)
+subset5 <- remaining
 
-train <- rbind(dataset.buggy[train.buggy,], dataset.notbuggy[train.notbuggy,])
-test <- rbind(dataset.buggy[test.buggy,], dataset.notbuggy[test.notbuggy,])
+################# End of Pre-processing and partition ################
 
-data.rf <- randomForest(as.factor(train$classes) ~., train, ntree=500, replace=T, mtry=4)
-pred <- predict(data.rf, test)
-cm <- table(observed=test$classes, predicted = pred)
-precision <- cm[4]/(cm[4]+cm[3])
-recall <- cm[4]/(cm[4]+cm[2])
-fmeasure <- 2*((precision*recall)/(precision+recall))
-precision
-recall
-fmeasure
+precision <- rep(0,5)
+recall <- rep(0,5)
+fmeasure <- rep(0,5)
+
+subset1.train <- sample(subset1, abs(0.90*length(subset1)), replace=F)
+subset1.test <- setdiff(subset1, subset1.train)
+data.rf <- randomForest(as.factor(dataset[subset1.train,ncol(dataset)]) ~., dataset[subset1.train,], ntree=500, replace=T, mtry=4)
+pred <- predict(data.rf, dataset[subset1.test,])
+cm <- table(observed=dataset[subset1.test,ncol(dataset)], predicted = pred)
+precision[1] <- cm[4]/(cm[4]+cm[3])
+recall[1] <- cm[4]/(cm[4]+cm[2])
+fmeasure[1] <- 2*((precision*recall)/(precision+recall))
+
+
+
