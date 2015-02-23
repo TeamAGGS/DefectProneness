06cead31bf43d4d41c168f1993cd68a41d838acd
Shivani <shivani.angane@gmail.com>
2015-02-23 00:24:53 -0500

    Serialized models from DecisionTree.R

--- code/Testing.bat
+++ code/Testing.bat
@@ -1,2 +1,2 @@
 set R_Script="C:\Program Files\R\R-3.1.2\bin\RScript.exe"
-%R_Script% Model-Testing.R ..\ant-model.rda ..\data\ant-1.7.csv ..\PredictedData\ant-1.7.csv > example.batch 2>&1
+%R_Script% Model-Testing.R ..\models\camel-training.csv.rda ..\data\camel-1.6.csv ..\PredictedData\camel-1.6.csv > testing.batch 2>&1
