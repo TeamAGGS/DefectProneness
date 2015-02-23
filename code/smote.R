3fa3f0a65233dd48d7b552d000b91e602f0ee51c
Apeksha Saxena <asaxena2@ncsu.edu>
2015-02-22 20:02:07 -0500

    Modified code to SMOTE on merged dataset of multiple versions.

--- code/smote.R
+++ code/smote.R
@@ -1,9 +1,13 @@
-dataset <- read.csv(file=choose.files(), na.strings=c(".", "NA", "", "?"), strip.white=TRUE, encoding="UTF-8")
+library(DMwR)
+#Merging n-1 versions for training data
+training_datasets <- choose.files(multi = TRUE)
+dataset <- do.call("rbind", lapply(training_datasets, read.csv, header = TRUE))
+
 dataset$bug <- factor(ifelse(dataset$bug > 0, 1, 0))
 dataset <- dataset[,-c(1,2,3)]
 class0 <- length(which(dataset$bug == 0))
 class1 <- nrow(dataset) - class0
 perc.over = (abs(class0 %/% class1)-1)*100
-newData <- SMOTE(bug ~., dataset, perc.over, perc.under=180, k=4)
+newData <- SMOTE(bug ~., dataset, perc.over, perc.under=135, k=4)
 table(newData$bug)
-write.csv(newData, file="data_SMOTE/kalkulator.csv", row.names=F)
+write.csv(newData, file = "training_data_SMOTE-2.22/ant-training.csv", row.names=FALSE)
