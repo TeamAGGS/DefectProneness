91cdd66a211866600246d78b121e56f1acc16da2
Gaurav Saraf <grv_srf@yahoo.co.in>
2015-02-25 19:11:10 -0500

    Generated results and fixed testing datasets. Removed first 3 columns from testing datasets that were copied directly from original data.

--- code/Model-Testing.R
+++ code/Model-Testing.R
@@ -16,7 +16,6 @@ result.filename <- args[3]
 load(file=model.filename)
 testdata <- read.csv(file=data.filename, na.strings=c(".", "NA", "", "?"), strip.white=TRUE, encoding="UTF-8")
 
-testdata <- testdata[,-c(1,2,3)]
 testdata$bug <- factor(ifelse(testdata$bug > 0, 1, 0))
 pred <- predict(bestmodel, testdata, type="class")
 cm <- confusion(pred, factor(testdata$bug, levels=c(0,1)))
--- code/Testing.bat
+++ code/Testing.bat
@@ -1,3 +1,11 @@
 set R_Script="C:\Program Files\R\R-3.1.2\bin\RScript.exe"
-%R_Script% Model-Testing.R ..\models\ant-training.csv.rda ..\data\ant-1.7.csv ..\PredictedData\ant-1.7.csv
+:: Write commands for all your datasets here
+%R_Script% Model-Testing.R ..\models\ivy-training.csv.rda ..\testing_data\ivy-2.0.csv ..\PredictedData\ivy-2.0.csv
+%R_Script% Model-Testing.R ..\models\jedit-training.csv.rda ..\testing_data\jedit-4.3.csv ..\PredictedData\ant-1.7.csv
+%R_Script% Model-Testing.R ..\models\kalkulator-training.csv.rda ..\testing_data\kalkulator-testing.csv ..\PredictedData\kalkulator.csv
+%R_Script% Model-Testing.R ..\models\log4j-training.csv.rda ..\testing_data\log4j-1.2.csv ..\PredictedData\log4j-1.2.csv
+%R_Script% Model-Testing.R ..\models\lucene-training.csv.rda ..\testing_data\lucene-2.4.csv ..\PredictedData\lucene-2.4.csv
+%R_Script% Model-Testing.R ..\models\nieruchomosci-training.csv.rda ..\testing_data\nieruchomosci-testing.csv ..\PredictedData\nieruchomosci.csv
+%R_Script% Model-Testing.R ..\models\pbeans-training.csv.rda ..\testing_data\pbeans2.csv ..\PredictedData\pbeans2.csv
+%R_Script% Model-Testing.R ..\models\pdftranslator-training.csv.rda ..\testing_data\pdftranslator-testing.csv ..\PredictedData\pdftranslator.csv
 pause
