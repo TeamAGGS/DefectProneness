cc4439231d57a13101b131c1e2ee067fd412a8df
Gaurav Saraf <grv_srf@yahoo.co.in>
2015-03-03 19:13:49 -0500

    Moved code-base to code-3.3 and created a new folder for code

---
+++ code-3.3/Model-Testing.R
@@ -0,0 +1,24 @@
+library(mda)
+
+# Clear the environment
+rm(list = ls())
+
+options(echo=TRUE) # if you want see commands in output file
+args <- commandArgs(trailingOnly = TRUE)
+print(args)
+
+# Get all the arguments
+model.filename <- args[1]
+data.filename <- args[2]
+result.filename <- args[3]
+
+# Read the model and the test-data
+load(file=model.filename)
+testdata <- read.csv(file=data.filename, na.strings=c(".", "NA", "", "?"), strip.white=TRUE, encoding="UTF-8")
+
+testdata$bug <- factor(ifelse(testdata$bug > 0, 1, 0))
+pred <- predict(bestmodel, testdata, type="class")
+cm <- confusion(pred, factor(testdata$bug, levels=c(0,1)))
+cm
+writedata <- cbind(testdata, pred)
+write.csv(writedata, file=result.filename, row.names=F)
