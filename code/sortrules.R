1c7fc8da67c9316710583913d1c855417cbcaea3
Gaurav Saraf <grv_srf@yahoo.co.in>
2015-03-20 23:31:51 -0400

    Code for rule-based model done. Need to add probability stuff.

---
+++ code/sortrules.R
@@ -0,0 +1,20 @@
+sortrules <- function(rules.all) {
+  # Can use Insertion Sort here since we have few rules
+  totalrules <- length(rules.all)
+ 
+  for(i in 2:totalrules) {
+    currentrule <- rules.all[[i]]
+    currentrl <- currentrule@rl
+    j <- i-1
+    while(j >= 1 && rules.all[[j]]@rl < currentrl) {
+      rules.all[[j+1]] = rules.all[[j]] 
+      j <- j-1
+    }
+    rules.all[[j+1]] = currentrule
+  }
+  
+  for(i in 1:totalrules) {
+    print(rules.all[[i]]@rl)
+  }
+  return(rules.all)
+}
