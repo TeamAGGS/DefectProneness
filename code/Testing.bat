93231b73ca69afe967655bb862c9a0b22dc24658
NorthCarolinaWolf <ywang95@ncsu.edu>
2015-03-27 10:15:34 -0400

    Test the newly generated models, close #58

--- code/Testing.bat
+++ code/Testing.bat
@@ -1,5 +1,53 @@
 set R_Script="C:\Program Files\R\R-3.1.2\bin\RScript.exe"
-:: Write commands for all your datasets here
+
+%R_Script% Model-Testing.R ..\models\ant-training.csv.rda ..\testing_data\ant-1.7.csv ..\PredictedData\ant-1.7.csv
+
+%R_Script% Model-Testing.R ..\models\arc-training.csv.rda ..\testing_data\arc-testing.csv ..\PredictedData\arc.csv
+
+%R_Script% Model-Testing.R ..\models\berek-training.csv.rda ..\testing_data\berek-testing.csv ..\PredictedData\berek.csv
+
+%R_Script% Model-Testing.R ..\models\camel-training.csv.rda ..\testing_data\camel-1.6.csv ..\PredictedData\camel-1.6.csv
+
+%R_Script% Model-Testing.R ..\models\ckjm-training.csv.rda ..\testing_data\ckjm-testing.csv ..\PredictedData\ckjm.csv
+
+%R_Script% Model-Testing.R ..\models\e-learning-training.csv.rda ..\testing_data\e-learning-testing.csv ..\PredictedData\e-learning.csv
+
+%R_Script% Model-Testing.R ..\models\forrest-training.csv.rda ..\testing_data\forrest-0.8.csv ..\PredictedData\forrest-0.8.csv
+
+%R_Script% Model-Testing.R ..\models\intercafe-training.csv.rda ..\testing_data\intercafe-testing.csv ..\PredictedData\intercafe.csv
+
+%R_Script% Model-Testing.R ..\models\ivy-training.csv.rda ..\testing_data\ivy-2.0.csv ..\PredictedData\ivy-2.0.csv
+
+%R_Script% Model-Testing.R ..\models\jedit-training.csv.rda ..\testing_data\jedit-4.3.csv ..\PredictedData\jedit-4.3.csv
+
+%R_Script% Model-Testing.R ..\models\kalkulator-training.csv.rda ..\testing_data\kalkulator-testing.csv ..\PredictedData\kalkulator.csv
+
+%R_Script% Model-Testing.R ..\models\log4j-training.csv.rda ..\testing_data\log4j-1.2.csv ..\PredictedData\log4j-1.2.csv
+
+%R_Script% Model-Testing.R ..\models\lucene-training.csv.rda ..\testing_data\lucene-2.4.csv ..\PredictedData\lucene-2.4.csv
+
+%R_Script% Model-Testing.R ..\models\nieruchomosci-training.csv.rda ..\testing_data\nieruchomosci-testing.csv ..\PredictedData\nieruchomosci.csv
+
+%R_Script% Model-Testing.R ..\models\pbeans-training.csv.rda ..\testing_data\pbeans2.csv ..\PredictedData\pbeans2.csv
+
+%R_Script% Model-Testing.R ..\models\pdftranslator-training.csv.rda ..\testing_data\pdftranslator-testing.csv ..\PredictedData\pdftranslator.csv
+
+%R_Script% Model-Testing.R ..\models\poi-training.csv.rda ..\testing_data\poi-3.0.csv ..\PredictedData\poi-3.0.csv
+
+%R_Script% Model-Testing.R ..\models\prop-training.csv.rda ..\testing_data\prop-6.csv ..\PredictedData\prop-6.csv
+
+%R_Script% Model-Testing.R ..\models\redaktor-training.csv.rda ..\testing_data\redaktor-testing.csv ..\PredictedData\redaktor.csv
+
+%R_Script% Model-Testing.R ..\models\serapion-training.csv.rda ..\testing_data\serapion-testing.csv ..\PredictedData\serapion-.csv
+
+%R_Script% Model-Testing.R ..\models\skarbonka-training.csv.rda ..\testing_data\skarbonka-testing.csv ..\PredictedData\skarbonka.csv
+
+%R_Script% Model-Testing.R ..\models\sklebagd-training.csv.rda ..\testing_data\sklebagd-testing.csv ..\PredictedData\sklebagd.csv
+
+%R_Script% Model-Testing.R ..\models\synapse-training.csv.rda ..\testing_data\synapse-1.2.csv ..\PredictedData\synapse-1.2.csv
+
+%R_Script% Model-Testing.R ..\models\systemdata-training.csv.rda ..\testing_data\systemdata-testing.csv ..\PredictedData\\systemdata.csv
+
 %R_Script% Model-Testing.R ..\models\szybkafucha-training.csv.rda ..\testing_data\szybkafucha-testing.csv ..\PredictedData\szybkafucha.csv
 
 %R_Script% Model-Testing.R ..\models\termoproject-training.csv.rda ..\testing_data\termoproject-testing.csv ..\PredictedData\termoproject.csv
