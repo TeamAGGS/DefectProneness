9e839a2ab13996d75665bef81914571df01ea1c0
Shivani <shivani.angane@gmail.com>
2015-04-06 00:35:18 -0400

    Added WHICH column and saving model based on the mortal/god value

--- code/modelWhich.R
+++ code/modelWhich.R
@@ -10,6 +10,8 @@ modelWhich <- function(train, test, threshold) {
   alpha <- 1
   beta <- 1000
   gamma <- 0
+  auc <- 0
+  bestrule <- ""
 
 #   Perform binning for all attributes and generate corresponding singleton rules
   for (attribute in (1:numattributes)) { 
@@ -46,8 +48,13 @@ modelWhich <- function(train, test, threshold) {
     triggered <- triggered[which(triggered[,att] > lower & triggered[,att] <= upper),]
   }
   
-  if(nrow(triggered) > 0) return(aucPdPf(test, triggered))
-  else return(0)
+  if(nrow(triggered) > 0) {
+    auc <- aucPdPf(test, triggered)
+    bestrule <- rule
+    #return(list(auc, triggered))
+  }
+  #else return(0)
+  return(list(auc, newrules[[1]]))
   
 #   for(i in 1:(length(newrules))) {
 #     rl <- newrules[[i]]@rl
